441ea4aed5fbee83c8e33f2a0724793f5e662338
beecloud <sistemas@beecloud.com.mx>
2025-01-23 13:20:56 -0600

    Se agrega webhook para API WA_BC_BUSSINESS

---
+++ public/webhook.php
@@ -0,0 +1,44 @@
+<?php
+
+	const TOKEN_BEECLOUD = "BEECLOUDAPIPHPMETA";
+	const WEBHOOK_URL = "https://beecloud.com.mx/webhook.php";
+
+	function verificarToken($req,$res){
+		try{
+			$token = $req['hub_verify_token'];
+			$challenge = $req['hub_challenge'];
+
+			if (isset($challenge) && isset($token) && $token == TOKEN_BEECLOUD){
+				$res->send($challenge);
+			} else {
+				$res->status(400)->send();
+			}
+		}catch(Exception $e){
+			$res->status(400)->send();
+		}
+	}
+
+	function recibirMensajes($req,$res){
+		try{
+			$res->send("EVENT_RECEIVED");
+		}catch(Exception $e){
+			$res->send("EVENT_RECEIVED");
+		}
+	}
+
+	if ($_SERVER['REQUEST_METHOD']==='POST'){
+		$input = file_get_contents('php://input');
+		$data = json_decode($input,true);
+
+		$recibirMensajes($data,http_response_code());
+	}else if($_SERVER['REQUEST_METHOD']==='GET'){
+
+		if (isset($_GET['hub_mode']) && isset($_GET['hub_verify_token']) && isset($_GET['hub_challenge']) && $_GET['hub_mode'] === 'suscribe' && $_GET['hub_verify_token'] === TOKEN_BEECLOUD) {
+			echo $GET['hub_challenge'];
+		} else {
+			http_response_code(403);
+		}
+		
+	}
+
+?>
