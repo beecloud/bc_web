2ca64a22ac030c0d2357f32beb1ae9366a0947b0
beecloud <sistemas@beecloud.com.mx>
2025-03-05 21:04:25 -0600

    Se agregan cambios a metawebhook

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,5 +12,7 @@ $routes->get('/', 'Landingpage::index');
 $routes->get('/landingpage', 'Landingpage::index');
 $routes->get('/privacidad', 'Landingpage::privacidad');
 $routes->get('/terminos-y-condiciones', 'Landingpage::terminos_y_condiciones');
+$routes->get('/metawebhook', 'Metawebhook::index');
+$routes->post('/metawebhook', 'Metawebhook::index');
 
 $routes->get('/muy-pronto', 'Muy_pronto::index');
---
+++ app/Controllers/Metawebhook.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Metawebhook extends BaseController
+{
+    public function index()
+    {
+        // API Meta 
+        const TOKEN_BEECLOUD = "BEECLOUDAPIPHPMETA";
+        const WEBHOOK_URL = "https://beecloud.com.mx/metawebhook";
+
+        function verificarToken($req,$res){
+            try{
+                $token = $req['hub_verify_token'];
+                $challenge = $req['hub_challenge'];
+
+                if (isset($challenge) && isset($token) && $token == TOKEN_BEECLOUD){
+                    $res->send($challenge);
+                } else {
+                    $res->status(400)->send();
+                }
+            }catch(Exception $e){
+                $res->status(400)->send();
+            }
+        }
+
+        function recibirMensajes($req,$res){
+            try{
+                $res->send("EVENT_RECEIVED");
+            }catch(Exception $e){
+                $res->send("EVENT_RECEIVED");
+            }
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $input = file_get_contents('php://input');
+            $data = json_decode($input,true);
+
+            $recibirMensajes($data,http_response_code());
+        }else if($_SERVER['REQUEST_METHOD']==='GET'){
+
+            if (isset($_GET['hub_mode']) && isset($_GET['hub_verify_token']) && isset($_GET['hub_challenge']) && $_GET['hub_mode'] === 'suscribe' && $_GET['hub_verify_token'] === TOKEN_BEECLOUD) {
+                echo $GET['hub_challenge'];
+            } else {
+                http_response_code(403);
+            }
+            
+        }
+
+    }
+}
+
+
+
